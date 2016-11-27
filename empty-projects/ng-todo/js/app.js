a1766dbcbb894f29da1a04dab9dc5c21eaa05f97
Austin Lanari <austin.lanari@gmail.com>
2016-11-27 14:04:54 -0500

    Fiddle with a few more things

--- empty-projects/ng-todo/js/app.js
+++ empty-projects/ng-todo/js/app.js
@@ -1,4 +1,4 @@
-var ngTodo = angular.module('ngTodo', [])
+var ngTodo = angular.module('ngTodo', ['StupidDirectives', 'StupidFilters'])
     .config(function() {
 
     });
--- empty-projects/ng-todo/js/directives.js
+++ empty-projects/ng-todo/js/directives.js
@@ -1,11 +1,12 @@
-angular.module('StupdDirectives', []).directive('completeColor', function() {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-            scope.$watch(attrs.completeColor, function (value) {
-                element.css('color', (value ? 'yellow' : 'limegreen'));
-            });
+angular.module('StupidDirectives', [])
+    .directive('completeColor', function() {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                scope.$watch(attrs.completeColor, function (value) {
+                    element.css('color', (value ? 'yellow' : 'limegreen'));
+                });
+            }
         }
-    }
 
 });
