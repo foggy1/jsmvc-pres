db69ba968763ab7fdcf9f3e25979a6fbc9a53b92
Austin Lanari <austin.lanari@gmail.com>
2016-11-27 13:36:40 -0500

    complete core functionality

--- empty-projects/ng-todo/js/ngTodoCtrl.js
+++ empty-projects/ng-todo/js/ngTodoCtrl.js
@@ -1,9 +1,23 @@
 ngTodo.controller('NgTodoCtrl', function NgTodoCtrl($scope) {
-  $scope.todos = [
-    { val: "A new cool thing", completed: false }
-    { val: "An old cool thing", completed: false }
-    { val: "Not a cool thing", completed: true }
-    { val: "Sort of a cool thing", completed: false }
-    { val: "A new *meh* thing", completed: true }
-  ];
+    $scope.todos = [
+        {val: "A new cool thing", completed: false},
+        {val: "An old cool thing", completed: false},
+        {val: "Not a cool thing", completed: true},
+        {val: "Sort of a cool thing", completed: false},
+        {val: "A new *meh* thing", completed: true}
+    ];
+
+    $scope.addNewTask = function () {
+        $scope.todos.unshift({completed: false, val: $scope.newTask});
+        $scope.newTask = '';
+    };
+
+    $scope.clearCompleted = function () {
+        $scope.todos = $scope.todos.filter(todo => !todo.completed);
+    };
+
+    $scope.removeTodo = function (index) {
+        $scope.todos.splice(index, 1);
+    };
+
 });
