cec131fdd0862001bc598d7d29242164eb90f980
Austin Lanari <austin.lanari@gmail.com>
2016-11-29 11:08:37 -0500

    add clear completed functionality

--- empty-projects/emb-todo/js/controllers.js
+++ empty-projects/emb-todo/js/controllers.js
@@ -10,6 +10,19 @@ Todos.TodoController = Ember.ObjectController.extend({
 
 Todos.TodosController = Ember.ArrayController.extend({
     actions: {
-        
+        createNewTodo: function () {
+            var newVal = this.get('newTodo');
+            var todo = this.store.createRecord('todo', {
+                val: newVal,
+                completed: false
+            });
+            this.set('newTodo', '');
+            todo.save();
+        },
+        clearCompleted: function () {
+            var completed = this.filterBy('completed', true);
+            completed.invoke('deleteRecord');
+            completed.invoke('save');
+        }
     }
 });
