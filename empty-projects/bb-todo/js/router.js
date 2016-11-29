c4ee1bdade0415ee2163fe34616ddf71f665fd3d
Austin Lanari <austin.lanari@gmail.com>
2016-11-29 13:00:07 -0500

    Remove semi-colon that was breaking everything

--- empty-projects/bb-todo/js/router.js
+++ empty-projects/bb-todo/js/router.js
@@ -1,7 +1,20 @@
 window.TodoApp = new (Backbone.Router.extend({
     routes: { '' : 'index' },
+    initialize: function () {
+        this.todoItems = new TodoItems();
+        this.todosView = new TodosView({collection: this.todoItems});
+        this.todosView.render();
+    },
     index: function () {
-
+        var fixtures = [
+            {val: "thing", completed: false},
+            {val: "stuff", completed: false},
+            {val: "jumanji", completed: true},
+            {val: "duuuuude", completed: true},
+            {val: "ayyyy", completed: false}
+        ];
+        $('#app').html(this.todosView.el);
+        this.todoItems.reset(fixtures);
     },
     start: function () {
         Backbone.history.start();
---
+++ empty-projects/bb-todo/js/views.js
@@ -0,0 +1,28 @@
+window.TodoView = Backbone.View.extend({
+    template: _.template('<span class="input-group-addon"><input <%= completed ? "checked=checked" : "" %> type="checkbox"></span><input value="<%= val %>" class="form-control<%= completed ? " finished" : "" %>" type="text"><span class="input-group-btn"><button class="btn btn-danger" type="button"><i class="glyphicon glyphicon-remove"></i></button></span>'),
+    render: function () {
+        this.$el.html(this.template(this.model.toJSON()));
+        return this
+    },
+    className: 'input-group input-group-lg'
+});
+
+window.TodosView = Backbone.View.extend({
+    initialize: function () {
+        this.collection.on('add', this.addOne, this);
+        this.collection.on('reset', this.addAll, this);
+        this.collection.on('destroy', this.render, this);
+    },
+    addOne: function (todoItem) {
+        var todoView = new TodoView({model: todoItem});
+        this.$el.append(todoView.render().el);
+    },
+    addAll: function () {
+        this.$el.empty();
+        this.collection.forEach(this.addOne, this);
+    },
+    render: function () {
+        this.addAll();
+        return this;
+    }
+});
